c825426e87cb87286d0a801312d5e8db3301ece2
Obulpathi <obulpathi@gmail.com>
2016-09-25 07:48:33 -0700

    tensorboard

--- 07.tensorflow/tensorboard.py
+++ 07.tensorflow/tensorboard.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-# Import data
+# import data
 from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
@@ -18,9 +18,10 @@ flags.DEFINE_string('data_dir', '/tmp/data/', 'Directory for storing data')
 
 mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
+# start the interactive session
 session = tf.InteractiveSession()
 
-# Create the model
+# create the model
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
@@ -29,21 +30,30 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 w_h = tf.histogram_summary("weights", W)
 b_h = tf.histogram_summary("biases", b)
 
-# Define loss and optimizer
+# define loss and optimizer
 y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 tf.scalar_summary("cost_function", cross_entropy)
 
-# Train
-tf.initialize_all_variables().run()
-summary_writer = tf.train.SummaryWriter('/tmp/tensorflow/logs', graph=session.graph)
+# define logging
+merge = tf.merge_all_summaries()
+summary_writer = tf.train.SummaryWriter('/tmp/tensorflow/logs', graph=tf.get_default_graph())
 
+tf.initialize_all_variables().run()
+
+# train
 for i in range(1000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
-  train_step.run({x: batch_xs, y_: batch_ys})
+  _, summary = session.run([train, merge], feed_dict={x: batch_xs, y_: batch_ys})
+  if i % 10 == 0:
+      summary_writer.add_summary(summary, i)
 
-# Test trained model
+# flush all buffers and cluse files
+summary_writer.flush()
+summary_writer.close()
+
+# test trained model
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
